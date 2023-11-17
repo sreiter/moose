f00178b2f4488a9e2912acf7fe38b1359ddb092c
Sebastian Reiter <sebastian.reiter@hexagon.com>
2023-11-17 17:16:08 +0100

    Serialization: Support `std::optional`.

---
+++ include/moose/stl/optional.h
@@ -0,0 +1,61 @@
+// This file is part of moose, a C++ serialization library
+//
+// Copyright (C) 2017 Sebastian Reiter, G-CSC Frankfurt <s.b.reiter@gmail.com>
+// Copyright (C) 2020-2022 Sebastian Reiter <s.b.reiter@gmail.com>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include <moose/archive.h>
+
+#include <optional>
+
+namespace moose
+{
+  template <class T>
+  void Serialize (
+    Archive& archive,
+    std::optional <T>& value)
+  {
+    if (archive.is_reading ())
+    {
+      bool hasValue = false;
+      archive ("hasValue", hasValue);
+      if (hasValue)
+      {
+        T tmp;
+        archive ("value", tmp);
+        value = tmp;
+      }
+      else
+      {
+        value = std::nullopt;
+      }
+    }
+    else
+    {
+      archive ("hasValue", value.has_value ());
+      if (value.has_value ())
+        archive ("value", *value);
+    }
+  }
+}// end of namespace
